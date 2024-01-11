79f879095000e001766da5c73d06d9000331d385
KimRPG <hunsung9909@gmail.com>
2024-01-11 16:08:25 +0900

    mapping

---
+++ mapping/src/main/java/com/jpa/mapping/entity/Student.java
@@ -0,0 +1,25 @@
+package com.jpa.mapping.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private long id;
+
+    private String name;
+    private String major;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    @JsonManagedReference
+    private University university;
+
+}
---
+++ mapping/src/main/java/com/jpa/mapping/entity/University.java
@@ -0,0 +1,26 @@
+package com.jpa.mapping.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "university_id")
+    private long id;
+
+
+    private String name;
+
+    @OneToMany(mappedBy = "university",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<Student> student;
+}
