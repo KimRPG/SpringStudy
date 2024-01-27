9637b68cef6db6baa5a3ba1baf1ac58706c40a22
KimRPG <hunsung9909@gmail.com>
2024-01-27 22:30:47 +0900

    v1

---
+++ line_backend/src/main/java/com/capstonexjapan/line_backend/linePay/line/controller/request/LineCreateRequest.java
@@ -0,0 +1,2 @@
+package com.capstonexjapan.line_backend.linePay.line.controller.request;public class LineCreateRequest {
+}
---
+++ line_backend/src/main/java/com/capstonexjapan/line_backend/linePay/line/controller/response/LineCreateResponse.java
@@ -0,0 +1,2 @@
+package com.capstonexjapan.line_backend.linePay.line.controller.response;public class LineCreateResponse {
+}
