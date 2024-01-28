3b1e4a59fe5acf963a6dabea06b7fc10896f9e7b
KimRPG <hunsung9909@gmail.com>
2024-01-28 19:10:29 +0900

    init : v2 구조

--- line_backend/src/main/java/com/capstonexjapan/line_backend/linePay/line/controller/request/LineCreateRequest.java
+++
@@ -1,2 +0,0 @@
-package com.capstonexjapan.line_backend.linePay.line.controller.request;public class LineCreateRequest {
-}
--- oauth2/src/main/java/com/pard/oauth2/v2/controller/V2Controller.java
+++
@@ -1,4 +0,0 @@
-package com.pard.oauth2.v2.controller;
-
-public class V2Controller {
-}
