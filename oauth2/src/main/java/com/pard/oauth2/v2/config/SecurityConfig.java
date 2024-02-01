838c8b27bba0e244402c7e8f27bb0ecbb88a2ae4
KimRPG <hunsung9909@gmail.com>
2024-02-01 21:35:23 +0900

    feat : SecurityConfig 완성

--- oauth2/src/main/java/com/pard/oauth2/v2/config/SecurityConfig.java
+++ oauth2/src/main/java/com/pard/oauth2/v2/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.pard.oauth2.v2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,6 +12,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityConfig securityConfig;
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//csrf 설정 꺼둠
+        http
+                .csrf((csrf) -> csrf.disable());
+        //form 로그인 방식 끔
+        http
+                .formLogin((login) -> login.disable());
+        //http basic 방식 끔
+        http
+                .httpBasic((basic) -> basic.disable());
+        //필터라 로그인 방식을 다 해줌
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        // "/" "/oauth/**" "/login/**"은 모두다 허용 그 외에는 로그인한 사람만 허용
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/oauth/**", "/login/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
 
 }
