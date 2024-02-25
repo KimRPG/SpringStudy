9098c214331f1051b8eec2257d57bdabeb8a6340
KimRPG <hunsung9909@gmail.com>
2024-02-25 22:52:34 +0900

    .

--- security/src/main/java/com/oauth/security/SecurityApplication.java
+++ security/src/main/java/com/oauth/security/SecurityApplication.java
@@ -2,7 +2,7 @@ package com.oauth.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//ㅅㅣ큐리티 화이팅
 @SpringBootApplication
 public class SecurityApplication {
 
