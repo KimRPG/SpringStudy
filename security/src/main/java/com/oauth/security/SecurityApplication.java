274ffc907eae50c454bd89633f190a89a80a6a35
KimRPG <hunsung9909@gmail.com>
2024-03-14 17:33:36 +0900

    .

--- security/src/main/java/com/oauth/security/SecurityApplication.java
+++ security/src/main/java/com/oauth/security/SecurityApplication.java
@@ -1,14 +1,14 @@
 package com.oauth.security;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//ㅅㅣ큐리티 화이팅
-//왜 안함?
+
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 
 }
---
+++ security/src/main/java/com/oauth/security/dto/OAuth2Response.java
@@ -0,0 +1,11 @@
+package com.oauth.security.dto;
+
+public interface OAuth2Response {
+    String getProvider();
+
+    String getProviderId();
+
+    String getEmail();
+
+    String getName();
+}
