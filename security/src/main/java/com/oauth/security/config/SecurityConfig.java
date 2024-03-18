eedcd83e98304c06f2aab78c3d9c6e0e038d85d3
KimRPG <hunsung9909@gmail.com>
2024-03-18 22:46:21 +0900

    .

--- security/src/main/java/com/oauth/security/config/SecurityConfig.java
+++ security/src/main/java/com/oauth/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.oauth.security.config;
 
+import com.oauth.security.jwt.JWTFilter;
+import com.oauth.security.jwt.JWTUtil;
+import com.oauth.security.oauth2.CustomSuccessHandler;
 import com.oauth.security.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +11,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService,
+                          CustomSuccessHandler customSuccessHandler,
+                          JWTUtil jwtUtil) {
         this.customOAuth2UserService = customOAuth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,10 +37,16 @@ public class SecurityConfig {
                 .formLogin(auth -> auth.disable());
         http
                 .httpBasic(auth -> auth.disable());
+
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
         http
                 .oauth2Login(oath2->oath2
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-                        .userService(customOAuth2UserService)));
+                        .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                );
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/").permitAll()
